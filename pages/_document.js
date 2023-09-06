4fe05763bfb808d091cf5de2653091ebef5a890e
davidstar20 <59632148+davidstar20@users.noreply.github.com>
2023-09-06 00:19:56 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,10 +23,51 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <style>
+            {`
+              .tabs {
+                display: flex;
+                justify-content: space-around;
+                padding: 10px;
+                background-color: #f5f5f5;
+              }
+              .tab-content {
+                display: none;
+                padding: 20px;
+                background-color: #e9e9e9;
+              }
+              .active {
+                display: block;
+              }
+            `}
+          </style>
         </Head>
         <body>
           <Main />
+          <div className="tabs">
+            <button onClick={() => this.showTab('download')}>Download Thumbnail</button>
+            <button onClick={() => this.showTab('tags')}>Find Video Tags</button>
+            <button onClick={() => this.showTab('privacy')}>Privacy Policy</button>
+          </div>
+          <div id="download" className="tab-content">
+            Content for downloading thumbnails.
+          </div>
+          <div id="tags" className="tab-content">
+            Content for finding video tags.
+          </div>
+          <div id="privacy" className="tab-content">
+            Your privacy policy content.
+          </div>
           <NextScript />
+          <script>
+            {`
+              function showTab(tabId) {
+                const contents = document.querySelectorAll('.tab-content');
+                contents.forEach(content => content.classList.remove('active'));
+                document.getElementById(tabId).classList.add('active');
+              }
+            `}
+          </script>
         </body>
       </Html>
     );
