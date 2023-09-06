2ebfd064e1639765af9b9c533acd63248304ca73
davidstar20 <59632148+davidstar20@users.noreply.github.com>
2023-09-06 01:09:40 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,9 +23,42 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <style>
+            {`
+              .step-guide {
+                padding: 20px;
+                background-color: #f5f5f5;
+                margin: 20px;
+                border-radius: 5px;
+              }
+              .step {
+                margin: 10px 0;
+                font-size: 18px;
+              }
+              .step-icon {
+                color: #4CAF50;
+                font-size: 24px;
+                margin-right: 10px;
+              }
+            `}
+          </style>
         </Head>
         <body>
           <Main />
+          <div className="step-guide">
+            <div className="step">
+              <span className="step-icon">🚀</span>
+              **Step 1**: Enter the Link - Begin by pasting your desired video link into the provided input field.
+            </div>
+            <div className="step">
+              <span className="step-icon">🖱️</span>
+              **Step 2**: Click "Download Thumbnails" - After entering your link, simply click the "Download Thumbnails" button.
+            </div>
+            <div className="step">
+              <span className="step-icon">🎉</span>
+              **Step 3**: Enjoy Your HD Thumbnail - Your high-definition thumbnail will be ready for download. Click the download link and get your image!
+            </div>
+          </div>
           <NextScript />
         </body>
       </Html>
@@ -34,5 +67,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
-
